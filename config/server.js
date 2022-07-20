523a3fcd1a8a56061f48731469e9821ce4413278
nahuelperafan <nahuelperafan@gmail.com>
2022-07-20 09:33:25 -0300

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '16c1ae7748fd224c3b338d9664dd20b5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a110ae4a-9dce-4f78-abb1-c4c34f2583dd'
+};
